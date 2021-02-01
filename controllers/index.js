801df98fa4b6f372c547b797ea7b91c91ec47280
Mansa Samlafo <msamlafo@gmail.com>
2021-02-01 01:07:25 -0500

    updated controller file names

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,10 @@
-const CommentController = require('./commentcontroller');
-const UserController = require('./usercontroller');
-const ProfileController  = require('./profilecontroller');
-const PoetryController = require('./poetrycontroller');
+const CommentController = require('./commentscontroller');
+const UserController = require('./userscontroller');
+const ProfileController  = require('./profilescontroller');
+const PoetryController = require('./poetryscontroller');
 const PublishingController = require('./publishingcontroller');
-const IssueFlaggingController = require('./issueflaggingcontroller');
-const LikeController = require('./likecontroller');
+const IssueFlaggingController = require('./issuescontroller');
+const LikeController = require('./likescontroller');
 
 module.exports = {
     CommentController,
